c7e3fc23b15c71863b40a828acda7d46fa6eb4b7
Summerscale, Emily <emily.summerscale20@imperial.ac.uk>
2026-05-02 18:19:58 +0100

    Fix issues

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,18 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+// ✅ Restore SPA route BEFORE React mounts
 const redirect = sessionStorage.getItem("spa_redirect");
 
 if (redirect) {
   sessionStorage.removeItem("spa_redirect");
-
-  // restore original route
   window.history.replaceState(null, "", redirect);
 }
-createRoot(document.getElementById("root")!).render(<App />);
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
